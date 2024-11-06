6fdcd12de41f049a84ac66d13fea9c288fb72be9
Sharkie C <sharkiecodes@gmail.com>
2024-11-06 14:34:08 +0100

    first commit

---
+++ VerticaleTuinen/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+  const int luchtSensorPin = 4;
+  const int vochtigheidSensorPin = 2;
+
+void setup() {
+pinMode(luchtSensorPin,INPUT);
+pinMode(vochtigheidSensorPin,INPUT);
+Serial.begin(115200);
+}
+
+void loop() {
+int lucht = analogRead(luchtSensorPin);
+int vochtigheid = analogRead(vochtigheidSensorPin);     //droog = 1600   in water = 700
+Serial.print("vochtigheid: ");  
+Serial.println(vochtigheid);
+Serial.print("lucht: ");  
+Serial.println(lucht);
+delay(300);
+}
